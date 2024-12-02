2446dfdf85b48ff3952987041e2ca744bccc1fa0
Marcos Gabriel <marcosindev@gmail.com>
2024-12-02 17:57:54 -0300

    Finalizando projeto pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 const listPokemons = document.getElementById('list-pokemons')
+const loadMoreButton = document.getElementById('loadMore')
 
-function convertPokemonToLi(pokemon) {
-    return `            
+const maxRecords = 151
+
+const limit = 8
+let offset = 0
+
+
+function loadPokemons(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemonList = []) => {
+        const newHtml = pokemonList.map((pokemon) => `            
             <li class="pokemon ${pokemon.type}">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
@@ -14,8 +22,24 @@ function convertPokemonToLi(pokemon) {
                 </div>
             </li>
             `
+        ).join('')
+        listPokemons.innerHTML += newHtml
+    })
 }
 
-pokeApi.getPokemons().then((pokemonList = []) => {
-    listPokemons.innerHTML += pokemonList.map(convertPokemonToLi).join('')
+loadPokemons(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemons(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemons(offset, limit)
+    }
 })
